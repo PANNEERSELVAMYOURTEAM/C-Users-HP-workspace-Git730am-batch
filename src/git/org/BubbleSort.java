00ca7a54c589b072584f111eda8c397653000b6a
baranishankar <baranisankar1994@gmail.com>
2019-04-25 08:34:18 +0530

    Modified Some class and added few class

---
+++ src/git/org/BubbleSort.java
@@ -0,0 +1,10 @@
+package git.org;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Added file for bubble sort");
+	}
+
+}
--- src/git/org/Fibonacciseries.java
+++ src/git/org/Fibonacciseries.java
@@ -2,46 +2,28 @@ package git.org;
 
 import java.util.Scanner;
 
-
-
 public class Fibonacciseries {
 
-
-
-    
 	public static void main(String[] args) {
+		System.out.println("This file is a Fibonacci series file");
 		int n;
-		 int a=0;
-		int b=0;
-		int c=1;
+		int a = 0;
+		int b = 0;
+		int c = 1;
 
 		@SuppressWarnings("resource")
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the value to print fibonacci series");
-		n= sc.nextInt();
+		n = sc.nextInt();
 		System.out.println("the fibonacci series for the entered value is ");
-	 for(int i=0;i<=n;i++){
-		 a=b;
-		 b=c;
-		 c=a+b;
-		 
-		 System.out.println(a);
-		 
-		 
-		 
-		 
-		 
-		 
-	 }
-		
-		
-		
-		
-		
+		for (int i = 0; i <= n; i++) {
+			a = b;
+			b = c;
+			c = a + b;
+
+			System.out.println(a);
+
+		}
+
 	}
-	}
-		       
-	
-
-
-
+}
